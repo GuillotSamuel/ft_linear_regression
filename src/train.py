fa2ee54078b2661e8bfc7f66bd98385ad5744180
Samuel Guillot <sguillot@paul-f3Br5s2.clusters.42paris.fr>
2024-11-20 20:35:28 +0100

    div

--- src/train.py
+++ src/train.py
@@ -1,18 +1,23 @@
 import csv
 import json
 
-def read_dataset(data_file):
+DATAS = "../data/data.csv"
+PARAMETERS = "../models/parameters.json"
+ITERATIONS = 1000
+LEARNING_RATE = 0.01
+
+def read_dataset():
     try:
         mileage = []
         price = []
-            with open(data_file, "r") as file:
-                input_file = csv.DictReader(file)
-                for row in input_file:
-                    mileage.append(float(row["km"]))
-                    price.append(float(row["price"]))
+        with open(DATAS, "r") as file:
+            input_file = csv.DictReader(file)
+            for row in input_file:
+                mileage.append(float(row["km"]))
+                price.append(float(row["price"]))
     except Exception as e:
         print(f"An error occurred: {e}")
-        exit 1
+        exit(1)
     return mileage, price
 
 
@@ -20,26 +25,27 @@ def ft_linear_regression(mileage, price):
     m = len(mileage)
     theta0 = 0
     theta1 = 0
+    
 
     
 
-def update_parameters(parameters_file, theta0, theta1)
+def update_parameters(theta0, theta1):
     try:
-        with open(parameters_file, "r") as file:
-            data = json.loads(file)
+        with open(PARAMETERS, "r") as file:
+            data = json.load(file)
         data["theta0"] = theta0
         data["theta1"] = theta1
-        with open(parameters_file, "w") as file:
-            file = json.dumps(data)
+        with open(PARAMETERS, "w") as file:
+            file = json.dump(data, file)
     except Exception as e:
         print(f"An error occured: {e}")
-        exit 1
+        exit(1)
 
 
 def main():
-    mileage, price = read_dataset("../data/data.csv")
+    mileage, price = read_dataset()
     theta0, theta1 = ft_linear_regression(mileage, price)
-    update_parameters("../models/parameters.json", theta0, theta1)
+    update_parameters(theta0, theta1)
 
 
 if __name__ == "__main__":
